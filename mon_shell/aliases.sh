359dfcbfc1799baaa20f93a71207cfd9c26a5f4c
1BoNoBo1 <boursicoton@gmail.com>
2025-08-06 12:42:50 +0100

    amelioration de bord setup et aliases.sh

--- mon_shell/aliases.sh
+++ mon_shell/aliases.sh
@@ -1,10 +1,21 @@
-# ~/.mon_setup/aliases.sh
-# Fichier d'alias pour le shell.
-# Chargé par le fichier .zshrc.
-# --------------------------------------------------
+#!/bin/bash
+# ------------------------------------------------------------
+# Script : ~/.mon_setup/aliases.sh
+# Installe et configure Borg Backup avec systemd
+# Dépôt chiffré local dans un dossier synchronisé avec kDrive
+# Usage : sudo ./borg_setup.sh [chemin/vers/repo/local]
+# Compatibilité : Ubuntu ≥ 24.04 / Debian ≥ 12 && bash/zsh
+# Auteur : Jean BoNoBo - 2025
+# ------------------------------------------------------------
+
 # ---------- alias de base ----------
 alias ll='ls -la --color=auto'          # Liste détaillée avec couleurs
 alias l='ls -CF'                        # Liste courte avec couleurs
+alias grep='grep --color=auto'          # grep avec couleurs
+alias cp='cp -i'                        # cp interactif pour éviter les écrasements
+alias mv='mv -i'                        # mv interactif pour éviter les écrasements
+alias rm='rm -i'                        # rm interactif pour éviter les suppressions accidentelles
+alias h='history'                       # Alias pour history
 # ========== fonctions système ==========
 
 alias reload='source ~/.zshrc && printf "${VERT}RECHARGEMENT SHELL${RESET}\n"'  # Alias pour recharger le shell avec un message de confirmation.
@@ -12,15 +23,3 @@ alias disque_UUID='lsblk -o NAME,SIZE,FSTYPE,TYPE,MOUNTPOINT,LABEL,UUID && '    
 alias disque_usage='df -hT && echo && sudo du -sh /home/*'                      # Affiche l'utilisation des disques et des répertoires home
 alias flatup='flatpak update -y && printf "${VERT}✔ Flatpak à jour${RESET}\n"'  # Met à jour les flatpaks avec un message de confirmation.
 alias snapup='sudo snap refresh && printf "${VERT}✔ Snap à jour${RESET}\n"'     # Met à jour les snaps avec un message de confirmation.
-
-
-# Aliases et Fonctions de Vérification pour Borg Backup
-
-# Aliases pratiques (à ajouter dans votre mon_shell ou ~/.bashrc)
-alias borg-backup-now='sudo systemctl start borg-backup.service'
-alias borg-backup-log='sudo tail -n 50 /var/log/borg_backup.log'
-alias borg-list='borg list /home/jim/kDrive/INFORMATIQUE/PC_TUF/borgrepo'
-alias borg-restore-last='LATEST=$(borg list /home/jim/kDrive/INFORMATIQUE/PC_TUF/borgrepo --format="{archive}\n" | tail -n1) && sudo /usr/local/sbin/borg_restore.sh "$LATEST"'
-alias borg-check-timer='systemctl status borg-backup.timer'
-alias borg-check-repo='ls -ld /home/jim/kDrive/INFORMATIQUE/PC_TUF/borgrepo'
-
